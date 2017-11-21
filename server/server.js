f5fd4065722cee3ce6dcb52eff0ffa3691693021
Jake Bray <jakebray@Megans-MacBook-Pro.local>
2017-11-21 10:25:43 -0600

    sending hard-coded array on shoes get request

--- server/server.js
+++ server/server.js
@@ -8,6 +8,13 @@ var port = 5000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static('server/public'));
 
+var shoes = [{name: 'nike', cost: '75'}]
+
+// for local host 5000/shoes should return array of shoe objects
+app.get('/shoes', function(req, res){
+    res.send(shoes);
+});
+
 app.listen(port, function(){
     console.log('server is listening on port', port);
 });
